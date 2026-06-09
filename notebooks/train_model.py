3015613247f7398430225bc080429b70d877f597
njek02 <n.schepers@student.rug.nl>
2026-06-09 15:44:27 +0200

    Added model architecture and training setup for DenseNet121

---
+++ notebooks/train_model.py
@@ -0,0 +1,96 @@
+import torchvision
+import torch
+import torch.nn as nn
+
+class DenseNet121(nn.Module):
+    
+    def __init__(self, num_classes):
+        super(DenseNet121, self).__init__()
+        self.densenet121 = torchvision.models.densenet121(pretrained=True)
+        num_ftrs = self.densenet121.classifier.in_features
+        self.densenet121.classifier = nn.Sequential(
+            nn.Linear(num_ftrs, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.densenet121(x)
+        return x
+
+
+def train_one_epoch(model, training_loader, loss_fn, optimizer, device):
+
+    running_loss = 0.
+    last_loss = 0.
+    
+    for i, data in enumerate(training_loader):
+        print(f"Batch {i+1}")
+        # Every data instance is an input + label pair
+        inputs, labels = data
+
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        # Zero your gradients for every batch!
+        optimizer.zero_grad()
+
+        # Make predictions for this batch
+        outputs = model(inputs)
+
+        # Compute the loss and its gradients
+        loss = loss_fn(outputs, labels)
+        loss.backward()
+
+        # Adjust learning weights
+        optimizer.step()
+
+        # Gather data and report
+        running_loss += loss.item()
+        if i % 100 == 99:
+            print(f'  batch {i + 1} avg loss: {running_loss / i}')
+
+    return running_loss / len(training_loader)
+
+
+def train_model(model, num_epochs, training_loader, validation_loader, device):
+    model.to(device)
+
+    loss_fn = nn.BCEWithLogitsLoss()
+ 
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+ 
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.1, patience=2)
+
+    best_vloss = 1_000_000.
+
+    for epoch in range(num_epochs):
+        print(f'EPOCH {epoch + 1}:')
+
+        # Make sure gradient tracking is on, and do a pass over the data
+        model.train(True)
+        avg_loss = train_one_epoch(model, training_loader, loss_fn, optimizer, device)
+
+        running_vloss = 0.0
+        # Set the model to evaluation mode, disabling dropout and using population
+        # statistics for batch normalization.
+        model.eval()
+
+        # Disable gradient computation and reduce memory consumption.
+        with torch.no_grad():
+            for i, vdata in enumerate(validation_loader):
+                vinputs, vlabels = vdata
+                
+                vinputs, vlabels = vinputs.to(device), vlabels.to(device)
+                
+                voutputs = model(vinputs)
+                vloss = loss_fn(voutputs, vlabels)
+                running_vloss += vloss.item()
+
+        avg_vloss = running_vloss / (i + 1)
+        print(f'LOSS train {avg_loss} valid {avg_vloss}')
+
+        scheduler.step(avg_vloss)
+
+        # Track best performance, and save the model's state
+        if avg_vloss < best_vloss:
+            best_vloss = avg_vloss
+            model_path = f'models/model_{epoch + 1}'
+            torch.save(model.state_dict(), model_path)
